714b15d9e60c70a7df4118e33282798b464aa428
Christine Pham <christine.pham.t@gmail.com>
2017-06-22 00:42:10 -0700

    created helper function to load all colors beforehand in order to ensure there are 10 unique colors being cycled through

--- frontend/browser_color.js
+++ frontend/browser_color.js
@@ -1,16 +1,25 @@
-function browser_color(){
+function load_colors() {
 	var colors = [];
-	var repeat = setInterval(function() {
-		var hue;
+	var hue;
+	while (colors.length < 10) {
 		hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-		if (colors.length < 10) {
-			if (!colors.includes(hue)) {
-				colors.push(hue);
-				document.body.style.backgroundColor = hue;
-			}
-		} else { 
+		if (!colors.includes(hue)) {
+			colors.push(hue);
+		}
+	}
+	return colors;
+}
+
+function browser_color() {
+	var colors = load_colors();
+	var count = 0;
+	var repeat = setInterval(function() {
+		if (count < 10) {
+			document.body.style.backgroundColor = colors[count];
+			count++;
+		} else {
 			clearInterval(repeat);
-		} 
+		}
 	}, 1000);
 }
 
